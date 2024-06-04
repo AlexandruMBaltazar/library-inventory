d0d883630cc5ad7a91f4b56690845d63ec83111f
xdesign-alexandrubaltazar <alexandru.baltazar@xdesign.com>
2024-06-04 14:23:31 +0100

    Set up avro schema and generate an event based on it

---
+++ library-events-producer/src/main/java/com/learnkafka/api/model/Book.java
@@ -0,0 +1,17 @@
+package com.learnkafka.api.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Data
+@Builder
+public class Book {
+    private Integer id;
+    private String name;
+    private String author;
+}
